7a9b1941f4504f432495809dfa3f1974b177c391
JunDevl <juninhoplay.pedone@gmail.com>
2025-09-15 23:05:57 -0300

    Fixed some bugs on the logic that checks the gameboard

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const reset = document.querySelector(".reset");
 const board = document.querySelector(".grid");
 
 const player1 = document.querySelector(".player1");
@@ -5,6 +6,8 @@ const player2 = document.querySelector(".player2");
 
 const player1Name = prompt("Please, inform the name of the first player:");
 const player2Name = prompt("Okay, now inform the name of the second player:");
+
+const blocks = document.querySelectorAll(".block");
 
 const Game = (() => {
   function generatePlayer(sign, name) {
@@ -16,9 +19,7 @@ const Game = (() => {
 
   let turn = "x";
 
-  let winner = null;
-
-  const gameboard = [];
+  let gameboard = [];
   for (let i = 0;i < 9;i++) gameboard.push(null);
 
   player1.textContent = `${Player1.name} Score: `;
@@ -33,50 +34,47 @@ const Game = (() => {
   player2score.textContent = Player2.score;
   player2.appendChild(player2score);
 
-  function checkGameStatus() {
+  function someoneDidScore() {
+    let horizontalConcurrentSigns = "";
     for (let horizontal_i = 0;horizontal_i < 9;horizontal_i++) {
-
-      let horizontalConcurrentSigns = "";
-      if (horizontal_i >= 3) {
-        horizontalConcurrentSigns = `${horizontalConcurrentSigns}${gameboard[horizontal_i]}`;
-        if ((horizontal_i / 2 === 1 || horizontal_i / 5 === 1 || horizontal_i / 8 === 1) && gameboard[horizontal_i - 3]) {
-          const initialSign = gameboard[horizontal_i - 3];
-          if (horizontalConcurrentSigns === `${initialSign}${initialSign}${initialSign}`) return Player1.sign === initialSign ? Player1.sign : Player2.sign;
-        };
+      if (horizontal_i < 3) {
+        let verticalConcurrentSigns = "";
+        const initialSign = gameboard[horizontal_i];
+        for (let vertical_i = horizontal_i;vertical_i <= horizontal_i + 6;vertical_i += 3) {
+          verticalConcurrentSigns = `${verticalConcurrentSigns}${gameboard[vertical_i]}`;
+          if ((vertical_i / 6 === 1 || vertical_i / 7 === 1 || vertical_i / 8 === 1) &&
+            initialSign &&
+            verticalConcurrentSigns === `${initialSign}${initialSign}${initialSign}`)
+            return Player1.sign === initialSign ? Player1.sign : Player2.sign;
+        }
       }
 
       if (horizontal_i === 0) {
         let diagonalConcurrentSigns = "";
         const initialSign = gameboard[horizontal_i];
-        for (let diagonal_i = horizontal_i;diagonal_i < 9;diagonal_i += 4) {
+        for (let diagonal_i = horizontal_i;diagonal_i <= 8;diagonal_i += 4) {
           diagonalConcurrentSigns = `${diagonalConcurrentSigns}${gameboard[diagonal_i]}`;
-          if (diagonal_i / 8 === 1 && gameboard[diagonal_i]) {
-            if (diagonalConcurrentSigns === `${initialSign}${initialSign}${initialSign}`) return Player1.sign === initialSign ? Player1.sign : Player2.sign;
-          }
+          if (diagonal_i / 8 === 1 && initialSign && diagonalConcurrentSigns === `${initialSign}${initialSign}${initialSign}`)
+            return Player1.sign === initialSign ? Player1.sign : Player2.sign;
         }
       }
 
       if (horizontal_i === 2) {
         let diagonalConcurrentSigns = "";
         const initialSign = gameboard[horizontal_i];
-        for (let diagonal_i = horizontal_i;diagonal_i < 7;diagonal_i += 2) {
+        for (let diagonal_i = horizontal_i;diagonal_i <= 6;diagonal_i += 2) {
           diagonalConcurrentSigns = `${diagonalConcurrentSigns}${gameboard[diagonal_i]}`;
-          if (diagonal_i / 6 === 1 && gameboard[diagonal_i]) {
-            if (diagonalConcurrentSigns === `${initialSign}${initialSign}${initialSign}`) return Player1.sign === initialSign ? Player1.sign : Player2.sign;
-          }
+          if (diagonal_i / 6 === 1 && initialSign && diagonalConcurrentSigns === `${initialSign}${initialSign}${initialSign}`)
+            return Player1.sign === initialSign ? Player1.sign : Player2.sign;
         }
       }
 
-      let verticalConcurrentSigns = "";
-      if (horizontal_i < 3) {
-        const initialSign = gameboard[vertical_i];
-        for (let vertical_i = horizontal_i;vertical_i < vertical_i + 7;vertical_i += 3) {
-          verticalConcurrentSigns = `${verticalConcurrentSigns}${gameboard[vertical_i]}`;
-          if ((vertical_i / 6 === 1 || vertical_i / 7 === 1 || vertical_i / 8 === 1) && gameboard[vertical_i - 6]) {
-            if (verticalConcurrentSigns === `${initialSign}${initialSign}${initialSign}`) return Player1.sign === initialSign ? Player1.sign : Player2.sign;
-          }
-        }
-      }
+      horizontalConcurrentSigns = `${horizontalConcurrentSigns}${gameboard[horizontal_i]}`;
+      const initialSign = gameboard[horizontal_i - 2];
+      if ((horizontal_i / 2 === 1 || horizontal_i / 5 === 1 || horizontal_i / 8 === 1) &&
+        initialSign &&
+        horizontalConcurrentSigns === `${initialSign}${initialSign}${initialSign}`)
+        return Player1.sign === initialSign ? Player1.sign : Player2.sign;
     }
   }
 
@@ -90,22 +88,25 @@ const Game = (() => {
 
   function updateGameboard(playerSign, blockElementID) {
     if (gameboard[+blockElementID - 1]) return;
+
     gameboard[+blockElementID - 1] = playerSign;
 
-    const playerScored = checkGameStatus();
-    if (playerScored) {
-      increasePlayerScore(playerScored);
-    };
     turn = turn === "x" ? "o" : "x";
+  }
+
+  function someoneScored() {
+    const playerScored = someoneDidScore();
+
+    if (!playerScored) return false;
+
+    increasePlayerScore(playerScored);
 
     player1score.textContent = Player1.score;
     player2score.textContent = Player2.score;
 
-    console.log(`${gameboard.slice(0, 2)}\n${gameboard.slice(3, 5)}\n${gameboard.slice(6, 8)}\n`);
-  }
+    gameboard = gameboard.map((item) => { return null; });
 
-  function getWinner() {
-    return winner;
+    return true;
   }
 
   function getCurrentTurn() {
@@ -116,12 +117,10 @@ const Game = (() => {
     Player1.score = 0;
     Player2.score = 0;
 
-    winner = null;
-
-    gameboard.map((item) => null);
+    gameboard = gameboard.map((item) => { return null; });
   }
 
-  return { getPlayerScore, increasePlayerScore, updateGameboard, getWinner, getCurrentTurn, reset };
+  return { getPlayerScore, increasePlayerScore, updateGameboard, someoneScored, getCurrentTurn, reset };
 })();
 
 const player1score = document.querySelector("#score-p1");
@@ -132,9 +131,14 @@ board.addEventListener("click", (e) => {
 
   if (e.target.textContent !== "") return;
 
-  console.log(e.target);
-
   e.target.textContent = Game.getCurrentTurn();
 
   Game.updateGameboard(e.target.textContent, e.target.id);
+
+  if (Game.someoneScored()) blocks.forEach((elem) => elem.textContent = "");
+});
+
+reset.addEventListener("click", () => {
+  Game.reset();
+  blocks.forEach((elem) => elem.textContent = "");
 });
